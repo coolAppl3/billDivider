cf8cc7c004eeb614480811c188c40f3413620504
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2023-12-05 03:51:21 +0100

    Patch 0.1

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  loginToken: {
+    type: String,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
