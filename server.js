24db5fefef82017d050065a585e9fd2f45fa6fca
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2023-12-10 02:34:10 +0100

    0.13
    Patch notes can be found in patch-notes.txt.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const connectDB = require('./config/db');
@@ -19,6 +20,9 @@ server.use(express.urlencoded({ extended: false }));
 
 // Importing Routers
 const userRouter = require('./routes/users');
+
+// Static Files
+server.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 server.use('/api/users', userRouter);
