57c67a0288db68b5ff8642d24cc882fa826ec1da
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2024-02-05 02:59:05 +0100

    feat: add standard-version to generate changelog

--- server.js
+++ server.js
@@ -38,19 +38,6 @@ const userRouter = require('./routes/users');
 // routes
 server.use('/api/users', userRouter);
 
-// ---
-
-// Homepage path
-// server.use('/', (req, res) => {
-//   res.json({ success: true, message: 'Homepage' });
-// });
-
-// Default path
-// server.use('*', (req, res) => {
-//   res.status(404).json({ success: false, message: 'Invalid path' });
-// });
-
-
 // Initialization
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
